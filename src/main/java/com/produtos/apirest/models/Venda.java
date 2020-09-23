d3954cba2d1d0d55679a7d311f5c9e3efd7aaad2
Gustavo Menezes <menezesguga@gmail.com>
2020-09-23 12:16:16 -0300

    Adcionando entidade de venda

--- src/main/java/com/produtos/apirest/models/Venda.java
+++ src/main/java/com/produtos/apirest/models/Venda.java
@@ -11,59 +11,55 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
-//classe dos produtos no banco
 @Entity
-@Table(name="TB_PRODUTO")
-public class Produto implements Serializable{
-	
-	private static final long serialVersionUID = 1L;
+@Table(name="TB_VENDA")
+public class Venda implements Serializable{
+
+private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	
-	private String nome;
-	
-	private BigDecimal quantidade;
-	
 	private BigDecimal valor;
 	
-	@ManyToMany(targetEntity=Venda.class)
-	private Set vendas;
+	private String date;
 	
-	
+	@ManyToMany(targetEntity=Produto.class)
+	private Set produtos;
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public BigDecimal getQuantidade() {
-		return quantidade;
-	}
-	public void setQuantidade(BigDecimal quantidade) {
-		this.quantidade = quantidade;
-	}
+
 	public BigDecimal getValor() {
 		return valor;
 	}
+
 	public void setValor(BigDecimal valor) {
 		this.valor = valor;
 	}
-	public Set getVendas() {
-		return vendas;
-	}
-	public void setVendas(Set vendas) {
-		this.vendas = vendas;
-	}
-	
-	
-	
 
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public Set getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(Set produtos) {
+		this.produtos = produtos;
+	}
+	
+	
+	
 }
